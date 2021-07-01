0cf9c96f9ef0074303be41f8650f1e9987202730
Nhật Long <69160592+bighero687@users.noreply.github.com>
2021-07-01 11:33:07 +0700

    update

---
+++ SHOPONLINE/Content/js/main.js
@@ -0,0 +1,10 @@
+﻿//Animation Form Login
+$("#btn__Login").click(function () {
+    $("#form__login").show();
+});
+//Hide form login
+$("#hide__form").click(hideForm);
+//Define function hideForm
+function hideForm() {
+    $("#form__login").hide();
+}
