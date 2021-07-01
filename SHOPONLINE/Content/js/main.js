42bdd80afb5ba2ad3d5d28b2a49f20ef0bbca254
Nhật Long <69160592+bighero687@users.noreply.github.com>
2021-07-01 18:59:36 +0700

    update

--- SHOPONLINE/Content/js/main.js
+++ SHOPONLINE/Content/js/main.js
@@ -4,7 +4,21 @@ $("#btn__Login").click(function () {
 });
 //Hide form login
 $("#hide__form").click(hideForm);
-//Define function hideForm
+//Define function hideForm login
 function hideForm() {
     $("#form__login").hide();
 }
+$("#btn__signup").click(function () {
+    $("#form__signup").show();
+    /*$("#form__login").hide();*/
+});
+$("#btn__login").click(function () {
+    /*$("#form__signup").hide();*/
+    $("#form__login").show();
+});
+/*//Hide form login
+$("#hide__form").click(hideForm);
+//Define function hideForm signup
+function hideForm() {
+    $("#form__signup").hide();
+}*/
